867ded8e29ca8773f85dda2b7f12fbcf054b63e2
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-12-12 14:31:36 -0700

    service worker const's added, 3 orker consts added, 3 sections needed

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/style.css",
+    "./js/index.js",
+    "./models/transaction.js"
+];
+
+
+// install portion
+
+
+
+
+
+// activate portion
+
+
+
+// fetch portion
