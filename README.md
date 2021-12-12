e8eb3b3df806ef216106be9a7b5ee30040dcf549
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-12-12 15:24:23 -0700

    service worker completed adn Readme updated

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Budget Tracker Starter Code
+# Budget Tracker 
+Web app that was converted to a PWA that allows users to sdd expenses/deposits to an existing budget without an internet connection. Offline additions to the budget will be updated when internet is available. 
+
+## Tech used
+- Indexed DB
+- Service workers
+- node.js
+
+## ScreenShot
+![budget pwa](https://user-images.githubusercontent.com/87105978/145731866-91a96ec3-a9df-4151-9188-ae5cf87f094c.png)
+
+## Install Dependencies
+- npm i 
+
--- public/service-worker.js
+++ public/service-worker.js
@@ -10,13 +10,45 @@ const FILES_TO_CACHE = [
 
 
 // install portion
-
-
-
-
-
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
 // activate portion
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        cache.keys().then(function (keylist) {
+            let cacheKeepList = keylist.filter(function (key){
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
 
-
-
+            return Promise.all(keylist.map(function (key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache :' + keylist[i]);
+                    return caches.delete(keylist[i]);
+                }
+            })
+        );
+        })
+    )
+})
 // fetch portion
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
