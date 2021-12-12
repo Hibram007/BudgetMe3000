2793ff669df3858c84d19a1aa9659215eea22c54
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-12-12 14:19:40 -0700

    idb.js file is completed and added

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,44 +1,65 @@
-// Code to Est. connection to IndexedDb
-
-// create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'budget_track' and set it to version 1
-const request = indexedDB.open('budget_track', 1);
 
-//Object Store event
-// this event will emit if the database version changes
-    // save a reference to the database 
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function (event) {
     const db = event.target.result;
-    // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_budget', { autoIncrement: true });
-  };
-  // upon a success
-request.onsuccess = function(event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
     db = event.target.result;
-  
-    // check if app is online, if yes run uploadBudget() function to send all local db data to api
+
     if (navigator.onLine) {
-      // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadBudget();
+        uploadTrans();
     }
-  };
-  
-  request.onerror = function(event) {
-    // log error here
-    console.log(event.target.errorCode);
-  };
+}
 
-  // code to Save Budget Data to IndexedDb
-
-// This function will be executed if we attempt to submit a new budget and there's no internet connection
+//this function will run when theres no internet connection when attempting a transaction
 function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['new_budget'], 'readwrite');
-  
-    // access the object store for `new_budget`
-    const budgetObjectStore = transaction.objectStore('new_budget');
-  
-    // add record to your store with add method
+    //open new transaction
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    //access the objectStore
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+    //use add method to add record to objectStore
     budgetObjectStore.add(record);
-  }
+}
+
+function uploadTrans() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        //send any data stored in IndexedDb to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    //open a new transaction and clear the objectStore
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const budgetObjectStore = transaction.objectStore('new_transaction');
+                    budgetObjectStore.clear();
+                    alert('All saved data has been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+//listen for app coming back online
+window.addEventListener('online', uploadTrans);
